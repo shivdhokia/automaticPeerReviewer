b5e025c34036f13d2eec17fe154dbf5091c52ceb
Shiv Dhokia <49102640+shivdhokia@users.noreply.github.com>
2020-02-06 18:59:52 +0000

    Added items to acitve and created pages
    Dropdown divs added which will include data about peer review.

--- main/script.js
+++ main/script.js
@@ -5,12 +5,52 @@ window.onload = function () {
     document.body.appendChild(clone);
 
     document.getElementById('loginBtn').onclick = function () {
-        document.body.removeChild(document.body.children[3]);
-        document.body.removeChild(document.body.children[2]);
+        const container = document.body.lastElementChild;
+        document.body.removeChild(container);
         let t = document.querySelector('#active-page');
         let clone = document.importNode(t.content, true);
         document.body.appendChild(clone);
+
+        var coll = document.getElementsByClassName("collapsible");
+        var i;
+
+        for (i = 0; i < coll.length; i++) {
+            coll[i].addEventListener("click", function () {
+                this.classList.toggle("active");
+                var content = this.nextElementSibling;
+                if (content.style.maxHeight) {
+                    content.style.maxHeight = null;
+                } else {
+                    content.style.maxHeight = content.scrollHeight + "px";
+                }
+            });
+        }
+
+        document.getElementById('createdButton').onclick = function () {
+            console.log("button clicked");
+            const container = document.body.lastElementChild;
+            document.body.removeChild(container);
+            let t = document.querySelector('#created-page');
+            let clone = document.importNode(t.content, true);
+            document.body.appendChild(clone);
+
+            var coll = document.getElementsByClassName("collapsible");
+            var i;
+    
+            for (i = 0; i < coll.length; i++) {
+                coll[i].addEventListener("click", function () {
+                    this.classList.toggle("active");
+                    var content = this.nextElementSibling;
+                    if (content.style.maxHeight) {
+                        content.style.maxHeight = null;
+                    } else {
+                        content.style.maxHeight = content.scrollHeight + "px";
+                    }
+                });
+            }
+        }
+
     }
 
-    
 }
+
