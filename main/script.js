4d04e8219d67e1e8f2fdb8f939d0a49a793bce79
Shiv Dhokia <49102640+shivdhokia@users.noreply.github.com>
2020-02-07 18:48:30 +0000

    made code lookk cleaner

--- main/script.js
+++ main/script.js
@@ -1,16 +1,6 @@
 window.onload = function () {
 
-    let t = document.querySelector('#login-page');
-    let clone = document.importNode(t.content, true);
-    document.body.appendChild(clone);
-
-    document.getElementById('loginBtn').onclick = function () {
-        const container = document.body.lastElementChild;
-        document.body.removeChild(container);
-        let t = document.querySelector('#active-page');
-        let clone = document.importNode(t.content, true);
-        document.body.appendChild(clone);
-
+    function collapsible() {
         var coll = document.getElementsByClassName("collapsible");
         var i;
 
@@ -25,29 +15,60 @@ window.onload = function () {
                 }
             });
         }
+    }
+
+    function loadTemplate(newPage) {
+        const container = document.body.lastElementChild;
+        document.body.removeChild(container);
+        let t = document.querySelector(newPage);
+        let clone = document.importNode(t.content, true);
+        document.body.appendChild(clone);
+    }
+
+    function loadActive(){
+        loadTemplate('#active-page');
+        collapsible();
+    }
+
+    function loadCreated() {
+        loadTemplate('#created-page');
+        collapsible();
+
+    }
+
+    function loadFormButtons() {
+        document.getElementById('addCohort').onclick = function () {
+
+            loadTemplate('#cohort_form_page');
+            document.getElementById('cancelCohort').onclick = function(){
+                loadCreated();
+            }
+        }
+
+        document.getElementById('addCriteria').onclick = function () {
+            loadTemplate('#criteriaForm');
+            document.getElementById('cancelCriteria').onclick = function() {
+                loadCreated();
+            }
+        }
+    }
+
+
+
+    let t = document.querySelector('#login-page');
+    let clone = document.importNode(t.content, true);
+    document.body.appendChild(clone);
+
+    document.getElementById('loginBtn').onclick = function () {
+
+        loadActive();
+
 
         document.getElementById('createdButton').onclick = function () {
-            console.log("button clicked");
-            const container = document.body.lastElementChild;
-            document.body.removeChild(container);
-            let t = document.querySelector('#created-page');
-            let clone = document.importNode(t.content, true);
-            document.body.appendChild(clone);
+            loadCreated();
+            collapsible();
+            loadFormButtons();
 
-            var coll = document.getElementsByClassName("collapsible");
-            var i;
-    
-            for (i = 0; i < coll.length; i++) {
-                coll[i].addEventListener("click", function () {
-                    this.classList.toggle("active");
-                    var content = this.nextElementSibling;
-                    if (content.style.maxHeight) {
-                        content.style.maxHeight = null;
-                    } else {
-                        content.style.maxHeight = content.scrollHeight + "px";
-                    }
-                });
-            }
         }
 
     }
