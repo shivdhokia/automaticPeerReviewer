0c0e4201432416dff7a343971410823901956246
Shiv Dhokia <49102640+shivdhokia@users.noreply.github.com>
2020-02-08 16:19:39 +0000

    created an express server.

---
+++ main/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express();
+
+/*app.get('/', (req, res) => {
+  res.send('Hello World!');
+  app.use(express.static('public'));
+});
+*/
+
+app.use('/', express.static('public'));
+app.listen(8000, () => {
+  console.log('Example app listening on port 8000!');
+});
