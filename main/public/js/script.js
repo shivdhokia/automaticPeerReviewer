8e28e666a517abe1dd35ba6e484743614b7e324d
Shiv Dhokia <49102640+shivdhokia@users.noreply.github.com>
2020-02-09 14:05:41 +0000

    added temp ajax requests

--- main/public/js/script.js
+++ main/public/js/script.js
@@ -8,6 +8,39 @@ window.onload = function () {
         document.body.appendChild(clone);
     }
 
+        // ajax get request need to fix
+        function loadXMLDoc() {
+            var xmlhttp = new XMLHttpRequest();
+    
+            xmlhttp.onreadystatechange = function () {
+                if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
+                    if (xmlhttp.status == 200) {
+                        document.getElementById("myDiv").innerHTML = xmlhttp.responseText;
+                    }
+                    else if (xmlhttp.status == 400) {
+                        alert('There was an error 400');
+                    }
+                    else {
+                        alert('something else other than 200 was returned');
+                    }
+                }
+            };
+    
+            xmlhttp.open("GET", "ajax_info.txt", true);
+            xmlhttp.send();
+        }
+    
+    
+        // ajax post request need to fix
+        function sendForm() {
+            xhttp.open("POST", "demo_post2.asp", true);
+            xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            xhttp.send("fname=Henry&lname=Ford");
+        }
+    
+    
+    
+
     // allows for collapsible divs.
     function collapsible() {
         var coll = document.getElementsByClassName("collapsible");
